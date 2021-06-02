66343ae32204f23dd85877d93b25bf2c226c0e3f
Fahmeda Hasan Fariha <58398404+fahmedahasanfariha@users.noreply.github.com>
2021-06-02 14:27:18 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$('.venobox').venobox(); 
+
+
+$('.service-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    centerMode: true,
+    centerPadding: "0",
+    arrows: true,
+    prevArrow: '<i class="fa fa-chevron-up prev"></i>',
+    nextArrow: '<i class="fa fa-chevron-down next"></i>',
+  });
