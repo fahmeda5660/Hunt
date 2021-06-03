54f3f2caaf8a903619212696238a221c273a11d5
Fahmeda Hasan Fariha <58398404+fahmedahasanfariha@users.noreply.github.com>
2021-06-03 13:14:34 +0600

    hunt2

--- js/script.js
+++ js/script.js
@@ -12,3 +12,13 @@ $('.service-slider').slick({
     prevArrow: '<i class="fa fa-chevron-up prev"></i>',
     nextArrow: '<i class="fa fa-chevron-down next"></i>',
   });
+$('.port-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+
+  });
+
